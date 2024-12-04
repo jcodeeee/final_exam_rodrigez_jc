84c82e60072d690cb2189f507e406825f95884f8
jcodeeee <janmer082003@gmail.com>
2024-12-04 17:13:45 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const navLinks = document.querySelector('.navlinks');
+    const toggleBtn = document.querySelector('.togglebtn');
+    navLinks.classList.toggle('open');
+    toggleBtn.classList.toggle('click');
+}
